94e3cf18d8be9ad1f8ab7c82ed9470d1f7f70676
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-04-16 11:41:59 -0400

    Refactor controller steps

--- tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
+++ tests/apps/mooc/backend/features/step_definitions/controller.steps.ts
@@ -1,15 +1,11 @@
 import assert from 'assert';
-import { AfterAll, BeforeAll, Given, Then } from 'cucumber';
+import { Given, Then } from 'cucumber';
 import request from 'supertest';
 
-import container from '../../../../../../src/apps/mooc/backend/dependency-injection';
-import { MoocBackendApp } from '../../../../../../src/apps/mooc/backend/MoocBackendApp';
-import { EnvironmentArranger } from '../../../../../Contexts/Shared/infrastructure/arranger/EnvironmentArranger';
+import { application } from './hooks.steps';
 
 let _request: request.Test;
-let application: MoocBackendApp;
 let _response: request.Response;
-let environmentArranger: EnvironmentArranger;
 
 Given('I send a GET request to {string}', (route: string) => {
 	_request = request(application.httpServer).get(route);
@@ -20,9 +16,7 @@ Then('the response status code should be {int}', async (status: number) => {
 });
 
 Given('I send a PUT request to {string} with body:', (route: string, body: string) => {
-	_request = request(application.httpServer)
-		.put(route)
-		.send(JSON.parse(body) as object);
+	_request = request(application.httpServer).put(route).send(JSON.parse(body));
 });
 
 Then('the response should be empty', () => {
@@ -31,20 +25,4 @@ Then('the response should be empty', () => {
 
 Then('the response content should be:', response => {
 	assert.deepStrictEqual(_response.body, JSON.parse(response));
-});
-
-BeforeAll(async () => {
-	environmentArranger = await container.get<Promise<EnvironmentArranger>>(
-		'Mooc.EnvironmentArranger'
-	);
-	environmentArranger.arrange();
-
-	application = new MoocBackendApp();
-	await application.start();
-});
-
-AfterAll(async () => {
-	await environmentArranger.close();
-
-	await application.stop();
 });
