ecb4a3e325c3b3e6ec323155d70bff1451d751c4
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-04-17 01:01:25 -0400

    Add validation for id field on Create Course

--- src/apps/mooc/backend/routes/courses.route.ts
+++ src/apps/mooc/backend/routes/courses.route.ts
@@ -6,7 +6,11 @@ import container from '../dependency-injection';
 import { validateReqSchema } from '.';
 
 export const register = (router: Router): void => {
-	const reqSchema = [body('name').exists().isString(), body('duration').exists().isString()];
+	const reqSchema = [
+		body('id').exists().isString(),
+		body('name').exists().isString(),
+		body('duration').exists().isString()
+	];
 
 	const coursePutController = container.get<CoursePutController>(
 		'Apps.mooc.controllers.CoursePutController'
